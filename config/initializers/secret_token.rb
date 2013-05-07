470977afec7e759fe639102c61d525c4d195fb29
sstarikov <starikov.by@gmail.com>
2013-05-07 17:10:32 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitkins::Application.config.secret_token = '3a8a05924983bc458079cbcdb3d143ebcc456bb0a3edcff9ff08cea55dfb9a0773c4b091f197ae70f8f470f54b0d175309e566843a414abba0648f552f7b4e21'
