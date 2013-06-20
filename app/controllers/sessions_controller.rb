516b222b91505fb49d4bf65b2809e4bad653870c
sstarikov <starikov.by@gmail.com>
2013-06-20 23:59:54 +0300

    update indents

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
-  def create_from_github
-    omniauth = request.env["omniauth.auth"]
-    user = User.find_by_github_uid(omniauth["uid"]) || User.create_from_omniauth(omniauth)
+    def create_from_github
+        omniauth = request.env["omniauth.auth"]
+        user = User.find_by_github_uid(omniauth["uid"]) || User.create_from_omniauth(omniauth)
 
-    session[:user_id] = user.id
+        session[:user_id] = user.id
 
-    redirect_to root_path, :notice => "Welcome, #{user.name}"
-  end
+        redirect_to root_path, :notice => "Welcome, #{user.name}"
+    end
 
-  def destroy
-    @current_user = session[:user_id] = nil
-    redirect_to root_path
-  end
+    def destroy
+        @current_user = session[:user_id] = nil
+        redirect_to root_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :github_uid, :name
+    attr_accessible :email, :github_uid, :name
 
-  def self.create_from_omniauth(omniauth)
-    User.new.tap do |user|
-      user.github_uid = omniauth["uid"]
-      user.name = omniauth["info"]["nickname"]
-      user.email = omniauth["info"]["email"]
-      user.save!
+    def self.create_from_omniauth(omniauth)
+        User.new.tap do |user|
+            user.github_uid = omniauth["uid"]
+            user.name = omniauth["info"]["nickname"]
+            user.email = omniauth["info"]["email"]
+            user.save!
+        end
     end
-  end
 end
